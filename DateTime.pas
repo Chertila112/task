67cff35b80bf7f212c5baa627d0084ef708ee38e
Evgeniy Sukhin <sukhin636@gmail.com>
2022-10-03 16:47:35 +0300

    Modified DateTime.pass

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,7 @@
 ﻿begin
-  
-end.
+  var n:=ReadInteger('введите год');
+  if (n mod 4 = 0) and (n mod 100 <>0) and (n mod 400 <>0) then
+    print($'{n} год - високосный')
+  else
+    print($'{n} год - не високосный');
+  end.
